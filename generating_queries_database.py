e95b2f887b5dcdcb0b62e44ba906dd2b5e2e9dc5
Hors <iliya.engineer20@gmail.com>
2024-05-26 17:47:27 +0500

    Переименование файла data_analysis_script.py в generating_queries_database.py и оптимизация кода

--- generating_queries_database.py
+++ generating_queries_database.py
@@ -7,7 +7,6 @@ from database import Database
 import time
 
 
-
 def formation_filters(query_: list, dates_=None, city=None):
     """
 
@@ -41,37 +40,45 @@ def formation_filters(query_: list, dates_=None, city=None):
     if len(overall_query) > 0:
         header_sort_column = ''
         attachment_sort_column = ''
-        result.append(db_query(header_sort_column, attachment_sort_column, dates_, city,overall_query))
+        result.append(db_query(header_sort_column, attachment_sort_column, dates_,
+                               city, overall_query))
 
     if len(developer_list) > len(framework_list) and framework_list:
         header_sort_column = 'required_framework'
         attachment_sort_column = 'developer_class'
-        result.append(db_query(header_sort_column, attachment_sort_column, dates_, city, framework_list, developer_list))
+        result.append(db_query(header_sort_column, attachment_sort_column,
+                               dates_, city, framework_list, developer_list))
 
     elif len(framework_list) > len(developer_list) and developer_list:
         header_sort_column = 'developer_class'
         attachment_sort_column = 'required_framework'
-        result.append(db_query(header_sort_column, attachment_sort_column, dates_, city, developer_list, framework_list))
+        result.append(
+            db_query(header_sort_column, attachment_sort_column,
+                     dates_, city, developer_list, framework_list))
 
     elif developer_list and framework_list and len(developer_list) == len(framework_list):
         header_sort_column = 'developer_class'
         attachment_sort_column = 'required_framework'
-        result.append(db_query(header_sort_column, attachment_sort_column, dates_, city, developer_list, framework_list))
+        result.append(
+            db_query(header_sort_column, attachment_sort_column,
+                     dates_, city, developer_list, framework_list))
 
     elif len(developer_list) > 0:
         header_sort_column = 'developer_class'
         attachment_sort_column = ''
-        result.append(db_query(header_sort_column, attachment_sort_column, dates_, city, developer_list))
+        result.append(db_query(header_sort_column, attachment_sort_column,
+                               dates_, city, developer_list))
 
     elif len(framework_list) > 0:
         header_sort_column = 'required_framework'
         attachment_sort_column = ''
-        result.append(db_query(header_sort_column, attachment_sort_column, dates_, city, framework_list))
+        result.append(db_query(header_sort_column, attachment_sort_column,
+                               dates_, city, framework_list))
     return result
 
 
-def db_query(header_sort_column: str, attachment_sort_column: str, dates_, city, sort_element_header: list,
-             sort_element_attachment: list = None, ):
+def db_query(header_sort_column: str, attachment_sort_column: str, dates_,
+             city, sort_element_header: list, sort_element_attachment: list = None, ):
     """
 
     :param header_sort_column:
@@ -95,7 +102,8 @@ def db_query(header_sort_column: str, attachment_sort_column: str, dates_, city,
     for hed in sort_element_header:
         if sort_element_attachment is not None:
             for attach in sort_element_attachment:
-                str_query += f"""{header_sort_column} LIKE '%{hed}%' AND {attachment_sort_column} LIKE '%{attach}%' {date_str} {city_str}"""
+                str_query += f"""{header_sort_column} LIKE '%{hed}%' AND
+{attachment_sort_column} LIKE '%{attach}%' {date_str} {city_str}"""
                 conditions = f'WHERE {str_query}'
                 query_db = db.select_data(table_name, filter_columns, conditions)
                 db_sum = sorted([int(val) for tup in query_db for val in tup[0].split(',')])
@@ -132,12 +140,12 @@ if __name__ == '__main__':
     # query = ['python developer']
     # query = ['django', 'fastapi', 'middle']
     # query = ['django', 'junior', 'middle']
-    # query = ['python developer','django', 'fastapi', 'middle']
+    query = ['python developer', 'django', 'fastapi', 'middle']
     # query = ['django', 'middle']
-    query = ['django', 'middle', 'senior', 'fastapi']
+    # query = ['django', 'middle', 'senior', 'fastapi']
     # query = ['middle']
     # query = ['django']
     r = formation_filters(query)
     finish = time.time() - start
-    print(r,len(r))
+    print(r, len(r))
     print(finish)
